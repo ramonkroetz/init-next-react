3198db1ca54b0a747a12fae547f365157ce7ca16
Ramon Kroetz <ramonkroetz@gmail.com>
2025-01-26 15:29:41 +0000

    install serve package

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
-import Image from 'next/image';
-import s from './page.module.css';
+"use client";
+import Image from "next/image";
+import s from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [asd, setASD] = useState("");
+
   return (
     <div className={s.page}>
-      <main className={s.main}>
-        <Image className={s.logo} src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
+      <main className={s.main} onClick={() => setASD("asd")}>
+        <Image
+          className={s.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
         <ol>
           <li>
             Get started by editing <code>app/page.tsx</code>.
