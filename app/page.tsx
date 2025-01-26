e7799bbf09596c09e46089cadb43f69b5ed97116
Ramon Kroetz <ramonkroetz@gmail.com>
2025-01-26 17:35:48 +0000

    next image optimizer

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,18 @@
-"use client";
-import Image from "next/image";
-import s from "./page.module.css";
-import { useState } from "react";
+import ExportedImage from 'next-image-export-optimizer';
+import s from './page.module.css';
 
 export default function Home() {
-  const [asd, setASD] = useState("");
-
   return (
     <div className={s.page}>
-      <main className={s.main} onClick={() => setASD("asd")}>
-        <Image
-          className={s.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+      <main className={s.main}>
+        <ExportedImage className={s.logo} src="/images/next.svg" alt="Next.js logo" width={180} height={38} priority />
         <ol>
           <li>
             Get started by editing <code>app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+        <ExportedImage src={'/images/example.png'} alt="Example Image." priority />
       </main>
     </div>
   );
--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,21 @@ const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
   },
+  images: {
+    loader: 'custom',
+    imageSizes: [],
+    deviceSizes: [360, 480, 768, 992, 1200, 1920],
+  },
+  transpilePackages: ['next-image-export-optimizer'],
+  env: {
+    nextImageExportOptimizer_imageFolderPath: 'public/images',
+    nextImageExportOptimizer_exportFolderPath: 'out',
+    nextImageExportOptimizer_quality: '80',
+    nextImageExportOptimizer_storePicturesInWEBP: 'true',
+    nextImageExportOptimizer_exportFolderName: 'nextImageExportOptimizer',
+    nextImageExportOptimizer_generateAndUseBlurImages: 'true',
+    nextImageExportOptimizer_remoteImageCacheTTL: '0',
+  },
 };
 
 export default nextConfig;
